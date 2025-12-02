7dd780cb64ec6ce8a98acd357416eca7f6ed74d6
Eric Laquer <LaquerEric@gmail.com>
2025-12-02 13:53:21 -0500

    introduced rung and manage

--- lib/git_steering/file_scanner.rb
+++ lib/git_steering/file_scanner.rb
@@ -8,16 +8,22 @@ module GitSteering
       @config = config
     end
 
-    # Scan vendor and submodules for steering files
+    # Scan vendor, bundled gems, and submodules for steering files
     # Returns hash: { source_path => relative_name }
     def scan_steering_files
       files = {}
       
-      # Scan vendor gems (higher priority)
+      # Scan vendor gems (highest priority)
       vendor_files = scan_directory(config.full_vendor_path)
       files.merge!(vendor_files)
       
-      # Scan submodules (lower priority, don't override vendor)
+      # Scan all bundled gems (medium priority, don't override vendor)
+      bundled_gem_files = scan_bundled_gems
+      bundled_gem_files.each do |source, name|
+        files[source] = name unless files.value?(name)
+      end
+      
+      # Scan submodules (lowest priority, don't override vendor or bundled gems)
       submodule_files = scan_directory(config.full_submodules_path)
       submodule_files.each do |source, name|
         files[source] = name unless files.value?(name)
@@ -28,11 +34,34 @@ module GitSteering
 
     private
 
+    def scan_bundled_gems
+      files = {}
+      
+      # Require bundler to access gem specs
+      begin
+        require 'bundler'
+        Bundler.load.specs.each do |spec|
+          gem_path = spec.full_gem_path
+          next unless Dir.exist?(gem_path)
+          
+          # Scan each gem for .kiro/steering/**/*.md files (including nested subdirectories)
+          Dir.glob(File.join(gem_path, ".kiro", "steering", "**", "*.md")).each do |source_path|
+            relative_name = File.basename(source_path)
+            files[source_path] = relative_name
+          end
+        end
+      rescue LoadError
+        # Bundler not available, skip scanning bundled gems
+      end
+      
+      files
+    end
+
     def scan_directory(base_path)
       files = {}
       return files unless Dir.exist?(base_path)
 
-      Dir.glob(File.join(base_path, "*", ".kiro", "steering", "*.md")).each do |source_path|
+      Dir.glob(File.join(base_path, "*", ".kiro", "steering", "**", "*.md")).each do |source_path|
         relative_name = File.basename(source_path)
         files[source_path] = relative_name
       end
